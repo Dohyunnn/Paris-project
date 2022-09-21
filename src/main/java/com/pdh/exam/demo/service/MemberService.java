5af5459f236cdb5a64d21c67e10540d953e61aae
Dohyunnn <qkrehgusqkrehgus@gmail.com>
2022-09-21 17:37:47 +0900

    C:/Program Files/Git/usr/member/doJoin에 ResultData 적용

--- src/main/java/com/pdh/exam/demo/service/MemberService.java
+++ src/main/java/com/pdh/exam/demo/service/MemberService.java
@@ -3,33 +3,37 @@ package com.pdh.exam.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.pdh.exam.demo.repository.MemberRepository;
+import com.pdh.exam.demo.utill.Ut;
 import com.pdh.exam.demo.vo.Member;
+import com.pdh.exam.demo.vo.ResultData;
 
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		// 로그인아이디 중복체크
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if ( oldMember != null ) {
-			return -1;
+
+		if (oldMember != null) {
+			return ResultData.from("F-7", Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId));
 		}
-	   
+
+		// 이름+이메일 중복체크
 		oldMember = getMemberByNameAndEmail(name, email);
 
 		if (oldMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email));
 		}
-		
-		
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
