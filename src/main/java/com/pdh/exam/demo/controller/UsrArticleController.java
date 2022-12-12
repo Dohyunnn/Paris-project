1476eb98dbce1d55712fcfd47973ff08877d7808
Dohyunnn <qkrehgusqkrehgus@gmail.com>
2022-12-12 18:43:17 +0900

    게시물 리스트에서 선택된 게시판에 관련된 게시물만 표시

--- src/main/java/com/pdh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/pdh/exam/demo/controller/UsrArticleController.java
@@ -41,7 +41,7 @@ public class UsrArticleController {
 			return rq.historyBackJsOnview(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
 		}
 		
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
 		
 
 		model.addAttribute("board", board);
